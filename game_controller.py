2570ceab7dc9bfc07ae2b10586a31d95b37b7e10
akuznetsova22 <109980023+akuznetsova22@users.noreply.github.com>
2022-07-26 14:23:04 +0200

    Add files via upload

---
+++ game_controller.py
@@ -0,0 +1,50 @@
+from view.game_view import GameView
+from model.game import Game
+from datetime import datetime
+
+class GameController:
+    """Game Controller class. Connects view with model
+    """
+    def __init__(self, view: GameView, model: Game) -> None:
+        self.view = view
+        self.model = model
+
+    def run_game(self): 
+        #creating the board with starting player positions and displaying it to the user   
+        self.model.initialize_board()
+        while True:
+            self.view.draw_board()
+            row, col = self.view.get_move(self.model.curr_player)
+            while not self.model.is_valid_move(row, col, self.model.curr_player):
+                #check if the move isnt valid due to no possible moves left:
+                #if yes - switch to another player. if not - offer user a hint
+                if len(self.model.get_available_moves(self.model.curr_player)):
+                    self.view.display_invalid_move()
+                    if self.view.display_options() == 'yes':
+                        print(self.model.show_moves(self.model.curr_player))
+                    row, col = self.view.get_move(self.model.curr_player)
+                else:
+                    self.view.no_moves()
+                    self.model.change_player()
+                    self.view.get_move(self.model.curr_player)
+            
+            self.model.make_move(row, col)
+            self.model.change_player()
+            #terminate game if not moves left for both players
+            if self.model.is_terminated():
+                break
+        #checks the winner of the game
+        player = self.model.check_winner()
+        if player:
+            self.view.display_winner(player)
+    
+    def save_winner(self):
+        winner = self.model.check_winner()
+        scores = self.model.keep_score()
+        date = datetime.now()
+        game_date = f'{date.year}-{date.month}-{date.day}  {date.hour}:{date.minute}'
+        with open('Reversi_scores.txt', 'w') as f:
+            print(f'Date: {game_date}, Winner: {winner}, Scores: {scores}', file = f)
+
+
+#Test
---
+++ main.py
@@ -0,0 +1,13 @@
+from model.game import Game
+from view.game_console_view import GameConsoleView
+from controller.game_controller import GameController
+
+
+model = Game(board_size=8)
+view = GameConsoleView(model)
+controller = GameController(view, model)
+
+controller.run_game()
+controller.save_winner()
+
+    
