7da1b9582d73ea1983d6107a3b8e452e9e314d71
Aleksandra Kuznetsova <a.kuznetcova.cass@gmail.com>
2022-08-05 16:45:56 +0200

    started GUI

--- GUI.py
+++ GUI.py
@@ -1,35 +1,42 @@
-# import tkinter as tk
-# from tkinter import scrolledtext
-# from model.game import Game
-# from view.game_console_view import GameConsoleView
-# from controller.game_controller import GameController
-# model = Game(board_size=8)
-# view = GameConsoleView(model)
-# controller = GameController(view, model)
+import tkinter as tk
+from tkinter import ttk
+from model.game import Game
+from view.game_console_view import GameConsoleView
+from controller.game_controller import GameController
+from view.game_view import GameView
+from model.game import Game
+from datetime import datetime
 
-# controller.run_game()
+def select_gm(option):
+    return int(option)
+
+
+model = Game(board_size=5)
+view = GameConsoleView(model)
+controller = GameController(view, model)
+
+# start_game = controller.run_game()
 # controller.end_game()
-# class ReversiGUI:
-#     def __init__(self, game, playerX, playerO):
-#         self.model = Game(board_size=8)
-#         self.view = GameConsoleView(model)
-#         self.controller = GameController(view, model)
-#         self.playerX = playerX
-#         self.playerO = playerO
-#         self.offset = 3
-#         self.cell_size = 40
 
-#         window = tk.Tk()
-#         window.wm_title("Reversi")
-#         window.lift()
-#         window.attributes("-topmost", True)
-#         self.window = window
-#         self.canvas = Canvas(window,height = self.cell_size * self.height + self.offset,width = self.cell_size * self.width + self.offset)
-#         self.move_label = Label(window)
-#         self.score_label = Label(window)
-#         self.text = scrolledtext.ScrolledText(root, width=70, height=10)
-#         self.move_label.pack(side="top")
-#         self.score_label.pack(side="top")
-#         self.canvas.pack()
-#         self.text.pack()
-#         self.draw_board()
+
+root = tk.Tk()
+root.title('Reversi Game')
+window_width = 300
+window_height = 450
+# get the screen dimension
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+# find the center point
+center_x = int(screen_width/2 - window_width / 2)
+center_y = int(screen_height/2 - window_height / 2)
+# set the position of the window to the center of the screen
+root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+greeting = tk.Label(text='Hello, Player. Welcome to Reversi\n\nSelect game mode below: ',\
+    foreground="yellow", background="purple", width=30, height = 20)
+greeting.pack()
+gm_button1 = ttk.Button(root,text='Player vs Player', command=lambda: select_gm(1)).pack()
+gm_button2 = ttk.Button(root,text='Player vs Computer. Easy', command=lambda: select_gm(2)).pack()
+gm_button3 = ttk.Button(root,text='Player vs Tought AI. Hardcore', command=lambda: select_gm(3)).pack()
+root.mainloop()
+
+
